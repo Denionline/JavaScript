35701907e9bea5b67d1e77bba6bc733d9f8dfbbf
Daniel Ximenes <93453046+Denionline@users.noreply.github.com>
2022-11-26 14:55:34 -0300

    up

---
+++ Curso em Vídeo - Curso JavaScript/Aula11/ex008.js
@@ -0,0 +1,8 @@
+var vel = 90
+
+console.log(`Sua velocidade atual é ${vel}Km/h.`)
+if (vel > 60) {
+    console.log(`Você ultrapassou o limite de velocidade. MULTADO!`)
+}
+
+console.log('Dirija sempre utilizando o cinto de segurança!')
