d30b36ab8209de20046df11681b68e31a7ce5508
Daniel Ximenes <93453046+Denionline@users.noreply.github.com>
2022-11-28 19:36:53 -0300

    up

---
+++ Curso em Vídeo - Curso JavaScript/Aula15/ambiente.js
@@ -0,0 +1,18 @@
+
+/*
+num.push(1)
+num.sort()
+console.log(num)
+console.log(`O vetor tem ${num.length} posições.`)
+console.log(`O primeiro valor do vetor é ${num[0]}.`)
+
+
+
+for (i = 0; i < num.length; i++){
+    console.log(num[i])
+}
+*/
+let num = [2, 5 , 8 ,6 ,7]
+for (let i in num){
+    console.log(num[i])
+}
