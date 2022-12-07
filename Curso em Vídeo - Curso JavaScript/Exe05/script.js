ce0a0be5abdd9ed9546067d13fdd602e5f48fce9
Daniel Ximenes <93453046+Denionline@users.noreply.github.com>
2022-12-07 19:40:52 -0300

    up

--- Curso em Vídeo - Curso JavaScript/Exe05/script.js
+++ Curso em Vídeo - Curso JavaScript/Exe05/script.js
@@ -1,8 +1,25 @@
-var formAdicionar = document.querySelector('#num')
-var numero = Number(formAdicionar.value)
-
-
-
 function adicionar(){
+    var formNum = document.querySelector('#num')
+    var numero = Number(formNum.value)
+    var item = document.createElement('option')
+    var res = document.querySelector('#res')
+    item.text = `Valor ${numero} adicionado.`
+    tab.appendChild(item)
+    var guardar = [numero]
     
+    if (guardar.length > 0){
+        guardar.push(numero)
+    }
 }
+
+function verificar(){
+    var formNum = document.querySelector('#num')
+    var numero = Number(formNum.value)
+    var item = document.createElement('option')
+    var res = document.querySelector('#res')
+
+    for (i=0;i<=guardar.length;i++){
+        res.innerHTML = guardar[i]
+    }
+}
+
