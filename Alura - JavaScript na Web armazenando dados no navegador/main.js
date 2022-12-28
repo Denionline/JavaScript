8d164fe14200cab8ba6607813526b065d49b259b
Daniel Ximenes <93453046+Denionline@users.noreply.github.com>
2022-12-28 14:47:42 -0300

    up

--- Alura - JavaScript na Web armazenando dados no navegador/main.js
+++ Alura - JavaScript na Web armazenando dados no navegador/main.js
@@ -1,11 +1,53 @@
 const form = document.querySelector('#novoItem')
+const lista = document.querySelector('#lista')
+const itens = JSON.parse(localStorage.getItem('itens')) || []
+
+
+itens.forEach( (element) => {
+    criarElemento(element)
+})
 
 form.addEventListener('submit', (evento) => {
     evento.preventDefault()
 
-    criarElemento(evento.target.elements['nome'].value, evento.target.elements['quantidade'].value)
+    const nome = evento.target.elements['nome']
+    const quantidade = evento.target.elements['quantidade']
+
+    const itemAtual = {
+        'nome': nome.value,
+        'quantidade': quantidade.value
+    }
+
+    const existe = itens.find(element => element.nome === nome.value)
+
+    if (existe){
+        itemAtual.id = existe.id
+    }else{
+        itemAtual.id = itens.length
+
+        criarElemento(itemAtual)
+
+        itens.push(itemAtual)
+    }
+
+
+    localStorage.setItem('itens', JSON.stringify(itens))
+
+    evento.target.elements['nome'].value = ""
+    evento.target.elements['quantidade'].value = ""
 })
 
-function criarElemento(nome, quantidade){
+function criarElemento(item){
     
+    const novoItem = document.createElement('li')
+    novoItem.classList.add('item')
+ 
+    const numeroItem = document.createElement('strong')
+    numeroItem.innerHTML = item.quantidade
+    numeroItem.dataset.id = item.id
+
+    novoItem.appendChild(numeroItem)
+    novoItem.innerHTML += item.nome
+
+    lista.appendChild(novoItem)
 }
