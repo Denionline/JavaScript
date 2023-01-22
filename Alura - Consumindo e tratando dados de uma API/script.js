b9c39fa7718c46a49065adce512f9dc641abb1c8
Daniel Ximenes <93453046+Denionline@users.noreply.github.com>
2023-01-21 21:26:06 -0300

    up

--- Alura - Consumindo e tratando dados de uma API/script.js
+++ Alura - Consumindo e tratando dados de uma API/script.js
@@ -1,3 +1,27 @@
-var consultaCEP = fetch('https://viacep.com.br/ws/01001000/json/')
-.then(resposta => resposta.json())
-.then(r => console.log(r))
+async function buscaEndereco (cep) {
+    var msgErro = document.getElementById('erro')
+    try{
+        var consultaCEP = await fetch(`https://viacep.com.br/ws/${cep}/json/`)
+        var consultaCEPConvertido = await consultaCEP.json()
+        if (consultaCEPConvertido.erro){
+            throw Error('CEP não existe!')
+        }
+        console.log(consultaCEPConvertido)
+        var endereco = document.getElementById('endereco')
+        var bairro = document.getElementById('bairro')
+        var cidade = document.getElementById('cidade')
+        var estado = document.getElementById('estado')
+
+        bairro.value = consultaCEPConvertido.bairro
+        endereco.value = consultaCEPConvertido.logradouro
+        cidade.value = consultaCEPConvertido.cidade
+        estado.value = consultaCEPConvertido.uf
+
+    }catch (erro){
+        msgErro.innerHTML = 'CEP inválido!'
+    }
+}
+
+var cep = document.getElementById('cep')
+cep.addEventListener('focusout', () => buscaEndereco(cep.value))
+
