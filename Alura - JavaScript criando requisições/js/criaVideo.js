a084f6f36959088266e3b2e6c9896b534c0da9be
Daniel Ximenes <93453046+Denionline@users.noreply.github.com>
2023-02-11 10:43:20 -0300

    up

--- Alura - JavaScript criando requisições/js/criaVideo.js
+++ Alura - JavaScript criando requisições/js/criaVideo.js
@@ -1,5 +1,8 @@
 const formulario = document.querySelector('[data-formulario]')
 
 function criarVideo (){
-    
+    const imagem = document.querySelector([data-imagem])
+    const url = document.querySelector([data-url])
+    const titulo = document.querySelector([data-titulo])
+    const descricao = Math.floor(Math.random * 10).toString()
 }
