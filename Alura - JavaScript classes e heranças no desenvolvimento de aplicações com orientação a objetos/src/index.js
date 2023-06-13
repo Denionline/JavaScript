a2bd18e013103a67a4f39a91853ef926ce31a79f
Daniel Ximenes <93453046+Denionline@users.noreply.github.com>
2023-06-12 21:20:58 -0300

    up

--- Alura - JavaScript classes e heranças no desenvolvimento de aplicações com orientação a objetos/src/index.js
+++ Alura - JavaScript classes e heranças no desenvolvimento de aplicações com orientação a objetos/src/index.js
@@ -1,23 +1,4 @@
 import { Personagem } from "./modules/personagem.js";
 
-// const personagemJose = {
-//     nome: 'Jose',
-//     vida: 7,
-//     mana: 6,
-//     level: 3,
-//     tipo: 'Arqueiro',
-// }
-
-const personagemPedrinho = new Personagem();
-personagemPedrinho.nome = 'Pedrinho';
-personagemPedrinho.mana = 12;
-personagemPedrinho.vida = 7;
-personagemPedrinho.tipo = 'Mago'
-personagemPedrinho.level = 5
-
-const personagemJose = new Personagem();
-personagemJose.nome = 'Jose';
-personagemJose.tipo = 'Arqueiro';
-personagemJose.level = 3;
-
-console.log(personagemJose.obterInsigna())
+const personagemPedrinho = new Personagem('Pedrinho', 5, 'Mago');
+const personagemJose = new Personagem('Jose', 3, 'Arqueiro')
